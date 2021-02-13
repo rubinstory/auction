7354d9c09f992e33b8cd99df23b3e03fde2b551e
Lee JunYoung <junyoung@ijun-yeong-ui-MacBook-Pro.local>
2021-02-13 20:37:40 +0900

    model admin add

--- web/auction/admin.py
+++ web/auction/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 from .models import Book, Person, BoardGame, Furniture, ETC
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Person)
-admin.site.register(BoardGame)
-admin.site.register(Furniture)
-admin.site.register(ETC)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ['name', 'category', 'price']
+
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ['student_id', 'name', 'major']
+
+class BoardGameAdmin(admin.ModelAdmin):
+	list_display = ['name', 'price']
+
+class FurnitureAdmin(admin.ModelAdmin):
+	list_display = ['name', 'price']
+
+class ETCAdmin(admin.ModelAdmin):
+	list_display = ['name', 'price']
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(BoardGame, BoardGameAdmin)
+admin.site.register(Furniture, FurnitureAdmin)
+admin.site.register(ETC, ETCAdmin)
